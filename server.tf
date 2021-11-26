e58cec491aafd4d974ae3f6eb86315329fb09873
divvaadarsh <95043495+divvaadarsh@users.noreply.github.com>
2021-11-26 20:00:38 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,12 +4,12 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.small"
-  key_name = "gds-import"
+  key_name = "ad-import"
 
   tags = {
-    Name = "Gagan-devops-server-v2"
+    Name = "adarsh-devops-server-v2"
     env = "production"
-    owner = "Gagandeep"
+    owner = "Adarsh"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
